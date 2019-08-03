e0076c4ab53710bf22e018d039737e31edaad2da
wmmxk <wmmkx@hotmail.com>
2019-08-03 12:09:31 -0700

    format float

---
+++ float_format/main.py
@@ -0,0 +1,11 @@
+iter=2.0
+tr_loss=0.43333
+val_loss=1.233344
+
+# the first field has 7 digits, so the width of the float is 7
+print("   iter|   train_loss|val_loss")
+print("------------------------------")
+
+print("%7.1f|%13.3f|%8.2f" %(iter, tr_loss, val_loss))
+print("%7.1f|%13.3f|%8.2f" %(iter, tr_loss, val_loss))
+print("%7.1f|%13.3f|%8.2f" %(iter, tr_loss, val_loss))
